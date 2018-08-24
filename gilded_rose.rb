945608268b61b4d2ca06c59bceb91493ccbf98d5
Tom <tom.w.page@gmail.com>
2018-08-24 17:04:30 +0200

    Basic refactor

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,21 +1,23 @@
-class GildedRose
+require './item'
 
+class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != 'Aged Brie' &&
+         item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+          if item.name != 'Sulfuras, Hand of Ragnaros'
             item.quality = item.quality - 1
           end
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -29,14 +31,14 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      if item.name != 'Sulfuras, Hand of Ragnaros'
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != 'Aged Brie'
+          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+              if item.name != 'Sulfuras, Hand of Ragnaros'
                 item.quality = item.quality - 1
               end
             end
@@ -50,19 +52,5 @@ class GildedRose
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
