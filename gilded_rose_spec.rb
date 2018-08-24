54e4e5763e1e789e63d08a3d8011d6671e070d33
Tom <tom.w.page@gmail.com>
2018-08-24 18:48:36 +0200

    Add normal item tests

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,33 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
+  subject { GildedRose.new(items) }
+
   describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].name).to eq 'foo'
+    context 'when the item is normal' do
+      let(:items) { [normal_item, no_sell_in_item] }
+      let(:normal_item) { Item.new('foo', 2, 2) }
+      let(:no_sell_in_item) { Item.new('foo', 0, 2) }
+
+      it 'does not change the name' do
+        expect { subject.update_quality }.not_to(change { items[0].name })
+      end
+
+      it 'degrades sellIn' do
+        expect { subject.update_quality }.to change { normal_item.sell_in }
+          .by(-1)
+      end
+
+      it 'degrades quality' do
+        expect { subject.update_quality }.to change { normal_item.quality }
+          .by(-1)
+      end
+
+      it 'degrades quality x2' do
+        expect { subject.update_quality }
+          .to change { no_sell_in_item.quality }
+          .by(-2)
+      end
     end
   end
 end
