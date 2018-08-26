d27b9aaa7bdf67ebac93a7048d9820e6cea5ec7f
Tom <tom.w.page@gmail.com>
2018-08-26 18:35:11 +0200

    Refactor further

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,12 +1,16 @@
 require './item'
 
 class GildedRose
+  BACKSTAGE_QUALITY = [0, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2].freeze
+
   def initialize(items)
     @items = items
   end
 
   def update_quality
     @items.each do |item|
+      reduce_sell_in(item)
+
       if item.name == 'Aged Brie'
         update_aged_brie(item)
       elsif item.name == 'Sulfuras, Hand of Ragnaros'
@@ -22,23 +26,22 @@ class GildedRose
   private
 
   def update_normal(item)
-    item.sell_in -= 1 if item.sell_in != 0
     item.quality -= 1 if item.sell_in >= 1
     item.quality -= 2 if item.sell_in.zero? && item.quality != 0
   end
 
   def update_aged_brie(item)
-    item.sell_in -= 1 if item.sell_in != 0
     item.quality += 1
   end
 
   def update_sulfuras(item)end
 
   def update_backstage(item)
-    item.sell_in -= 1 if item.sell_in != 0
-    item.quality += 1 if item.sell_in >= 11
-    item.quality += 2 if item.sell_in <= 10 && item.sell_in >= 5
-    item.quality += 3 if item.sell_in <= 4 && item.quality != 0
+    item.sell_in >= 11 ? item.quality += 1 : item.quality += BACKSTAGE_QUALITY[item.sell_in]
     item.quality = 0 if item.sell_in.zero?
+  end
+
+  def reduce_sell_in(item)
+    item.sell_in -= 1 if item.sell_in != 0
   end
 end
